e626cc54860216ac3a44fcae58ad09d3d42d858f
Mislam23 <islam.mohammad.student@gmail.com>
2022-05-08 00:40:55 -0400

    connected frontend react to backend postgres in formInput

--- client/src/components/TodoInput.js
+++ client/src/components/TodoInput.js
@@ -3,12 +3,27 @@ import React, {Fragment, useState} from "react";
 export default function TodoInput() {
 
   const [description, setDescription] = useState("");
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const body = {description};
+      const response = await fetch("http://localhost:4000/todos", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      });
+      console.log(response)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
 
   return (
     <Fragment>
       <h1 className="text-center mt-5 display-1">Todo List</h1>
-      <form className="d-flex">
-        <imput type="text" className="form-control"/>
+      <form className="d-flex" onSubmit={onFormSubmit}>
+        <input type="text" className="form-control" placeholder="What's on your mind?" value={description} 
+        onChange={event => setDescription(event.target.value)}/>
         <button className="btn btn-outline-info">Add to List</button>
       </form>
     </Fragment>
