258b4491699d6bbbd1edce4657d12cd872bee952
Mislam23 <islam.mohammad.student@gmail.com>
2022-05-08 20:41:33 -0400

    possible bug found

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const devConfig = {
 }; 
 
 const proConfig = {
-  connectionString: processenv.DATABASE_URL
+  connectionString: process.env.DATABASE_URL
 }
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
