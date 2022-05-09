514fb3028bcb97bbd3993b80fde9c04ddcd07b6e
Mislam23 <islam.mohammad.student@gmail.com>
2022-05-08 21:12:26 -0400

    finding bug

--- client/src/components/TodoInput.js
+++ client/src/components/TodoInput.js
@@ -7,7 +7,7 @@ const TodoInput = () => {
     event.preventDefault();
     try {
       const body = {description};
-      const response = await fetch("/todos", {
+      const response = await fetch('/todos', {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
