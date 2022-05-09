e43408e8e9ec4590950c14a31949026c6d0b08c7
Mislam23 <islam.mohammad.student@gmail.com>
2022-05-08 21:29:59 -0400

    working on bug

--- client/src/components/TodoInput.js
+++ client/src/components/TodoInput.js
@@ -12,7 +12,7 @@ const TodoInput = () => {
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify(body)
       });
-      window.location = "https://fullstack-crud-todolist-webapp.herokuapp.com/";
+      window.location = "/";
       // console.log(response)
     } catch (err) {
       console.error(err.message)
